8cd0e8921355e54747f130fdba46a4f569b926ab
Luis Felipe <luisfelipe91_gzz@hotmail.com>
2019-06-15 11:41:45 -0500

    iniciando nuestro proyecto

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// mando a llamar la funcion mensaje
+function load(){
+    // alert("Listo");
+    Mensaje("hola Mundo");
+}
+// creamos la funcion mensaje
+function Mensaje(saludo){
+    alert(saludo);
+}
+
---
+++ src/consulta.js
@@ -0,0 +1,9 @@
+// mando a llamar la funcion mensaje
+function load(){
+    // alert("Listo");
+    Mensaje("hola Mundo");
+}
+// creamos la funcion mensaje
+function Mensaje(saludo){
+    alert(saludo);
+}
